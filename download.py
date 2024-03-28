00f8979167162f8c0c67272aff7c1d95066948a0
dyllisLev <dyllis.lev@gmail.com>
2024-03-28 13:25:52 +0900

    Update download.py
    수정

--- download.py
+++ download.py
@@ -34,6 +34,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 
 #########################################################
